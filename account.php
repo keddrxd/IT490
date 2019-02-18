fd6aa57b01ce7eb94add118a3494b38c162e94e0
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-02-18 10:58:15 -0500

    Add files via upload

---
+++ account.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "192.168.1.11";
+$username = "Admin";
+$project = "Users";
+$password = "Admin";
+?>
---
+++ testRabbitMQServer.php
@@ -0,0 +1,86 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+
+include ("account.php");
+
+function doLogin($username,$password)
+{
+    // lookup username in databas
+    // check password
+    return true;
+    //return false if not valid
+}
+
+function authentication($username,$password)
+{
+	
+	global $userdatabase;
+	$s = "SELECT * from Users where User = \"$username\" && Password = \"$password\"";
+	$t = mysqli_query($Users, $s);
+	
+	if (mysqli_num_rows($t) == 0)
+	{
+		echo "Username and password incorrect.");
+		return false;
+	}
+	else
+	{
+		echo "Username and password correct.");
+		return true;
+	}
+	
+	
+	
+}
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
