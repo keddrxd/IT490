b35400b4e745f773778ebce2260e242052801459
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-02-18 12:04:07 -0500

    Add files via upload

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -1,13 +1,13 @@
 #!/usr/bin/php
-
 <?php
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-use PhpAmqpLib\Connection\AMQPMessage;
-#use PhpAmqpLib\Message\AMQPMessage;
+
 
 include ("account.php");
+
+$userdatabase = mysqli_connect($hostname, $username, $password, $project);
 
 function doLogin($username,$password)
 {
@@ -34,7 +34,23 @@ function authentication($username,$password)
 		echo "Username and password correct.";
 		return true;
 	}
-	
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}	
 	
 	
 }
