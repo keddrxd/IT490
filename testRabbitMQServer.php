78e3051e9293a7bd64d500466eaa1c52a8d255ff
Adam <ak866@njit.edu>
2019-02-18 12:01:55 -0500

    Changed

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -5,7 +5,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 use PhpAmqpLib\Connection\AMQPMessage;
-use PhpAmqpLib\Message\AMQPMessage;
+#use PhpAmqpLib\Message\AMQPMessage;
 
 include ("account.php");
 
