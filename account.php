421b85b0e80d58130f01eee633c2cd99df7be313
Adam <ak866@njit.edu>
2019-02-25 17:41:05 -0500

    yes

--- account.php
+++ account.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = "192.168.1.10";
-$username = "Admin";
-$project = "Users";
-$password = "Admin";
+$hostname = "192.168.1.6";
+$username = "adminn";
+$database = "usersDB";
+$password = "adminPwd";
 ?>
