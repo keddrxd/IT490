ba20f9a3ed636b0e3b18ef5d114d4e07ce857496
Andrew <asr56@njit.edu>
2019-02-25 12:09:03 -0500

    Fixed php

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -16,7 +16,8 @@ if (mysqli_connect_errno())
   {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
       exit();
-  }
+}
+
 echo "Successfully connected to MySQL.".PHP_EOL;
 
 function doLogin($username,$password)
