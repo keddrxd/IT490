505c32507b6876c06a4575c3e27052e82419c5b9
Andrew <asr56@njit.edu>
2019-02-25 12:25:05 -0500

    changed back php

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -14,7 +14,7 @@ global $userdatabase;
  */
 if (mysqli_connect_errno())
   {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      echo "Failed to connect to MySQL: " . mysqli_connect_errno();
       exit();
 }
 
