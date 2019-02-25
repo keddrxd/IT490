a74b573d74024d3fcba1893411e0b8e4cbd1a604
Andrew <asr56@njit.edu>
2019-02-25 16:39:28 -0500

    Changed PHP

--- testServer.php
+++ testServer.php
@@ -1,4 +1,4 @@
-!#usr/bin/php
+#!usr/bin/php
 <?php
 
 require_once('path.inc');
@@ -10,18 +10,18 @@ function connectSQL()
 {
 	$db = new mysqli('192.168.1.8','Admin','Admin','Users');
 	
-	if ($db ->errno !=0)
+	if ($db -> errno !=0)
 	{
-		echo "Cannot connect to mySQL: ".$db->error . PHP_EOL;
+		echo "Cannot connect to mySQL: ".$db->error . PHP_E0L;
 		exit(0);
 	
 	}
-	echo "Connected to mySQL Database".PHP_EOL;
+	echo "Connected to mySQL Database".PHP_E0L;
 }
 
 function requestProcessor($request)
 {
-	echo "received request".PHP_EOL;
+	echo "received request".PHP_E0L;
 	var_dump($request);
 	if(!isset($request['type']))
 	{
@@ -37,15 +37,14 @@ function requestProcessor($request)
 	return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
-$server - new rabbitMQServer("testRabbitMQ.ini","testServer");
-echo "RabbitMQ Server Began".PHP_EOL;
+$server =  new rabbitMQServer("testRabbitMQ.ini","testServer");
+echo "RabbitMQ Server Began".PHP_E0L;
 $server->process_requests('requestProcessor');
-echo "RabbitMQ Server Ended".PHP_EOL;
+echo "RabbitMQ Server Ended".PHP_E0L;
 exit();
 	
 	
 	
-}
 
 
 
