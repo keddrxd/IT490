27e93d0bf218e5f1acc18f7786234b87da98613f
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-03-04 18:37:08 -0500

    Update login1.php

--- login1.php
+++ login1.php
@@ -1,7 +1,7 @@
 <?php
 
 include(__DIR__ . 'vendor/autoload.php');
-use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 $host = 'localhost';
