11f4c82886fc6c597568df50d11b2bcd8357bd1a
Adam Kedra <47116358+keddrxd@users.noreply.github.com>
2019-03-04 18:20:04 -0500

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$host = '192.168.1.4';
+$port = 5672;
+$user = 'adam';
+$pass = 'adam';
+$vhost = 'beHost';
+$exchange = 'beExchange';
+$queue = "beQueue";
+
+$connection = new AMQPStreamConnection($host,$port,$user,$pass,$vhost);
+$channel = $connection->channel();
+
+$channel->queue_declare($queue,true,true,false,false);
+$channel->exchange_declare($exchange,'direct',false,true,false);
+$channel->queue_bind($queue,$exchange);
+
+$messageBody = json_decode(['user' => 'adam', 'password' => 'adam',]);
+
+
+
+?>
